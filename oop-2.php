8354f8785ff87fc054d8f95bb6e57f6bdb712ff6
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-01-28 19:51:14 +0100

    CatToy completato (stessa struttura di DogToy)

--- oop-2.php
+++ oop-2.php
@@ -89,3 +89,30 @@ class DogToy extends DOG
         $this->playStyle = $playStyle;
     }
 }
+
+// classe estesa per giochi per il gatto 
+class CatToy extends CAT
+{
+    public $ageGroup;
+    public $material;
+    public $interactive;
+    public $durability;
+    public $playStyle;
+
+    public function __construct($name, $price, $type, $brand, $ageGroup, $material, $interactive, $durability, $playStyle)
+    {
+        parent::__construct($name, $price, $type, null, $brand, null, null, null, null, null);
+        $this->ageGroup = $ageGroup;
+        $this->material = $material;
+        $this->interactive = $interactive;
+        $this->durability = $durability;
+        $this->playStyle = $playStyle;
+    }
+}
+
+// // classe estesa per cucce DOG 
+
+
+
+
+// classe estesa per cucce CAT
