54ee6c9a3aa0c77296a9ba29bd43df75b4b90e61
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-01-28 19:20:04 +0100

    variabili inizializzate

---
+++ oop-2.php
@@ -0,0 +1,14 @@
+<?php
+class ITEMS
+{
+    // variabili 
+    public $name;
+    public $price;
+    public $category;
+    public $type;
+    public $size;
+    public $brand;
+    public $quantityInStock;
+    public $description;
+    public $imageUrl;
+}
