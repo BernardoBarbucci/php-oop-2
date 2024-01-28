fafb02986a5c36f1b50952dc750ef33d1ffefc8b
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-01-28 19:24:26 +0100

    construct completato

--- oop-2.php
+++ oop-2.php
@@ -11,4 +11,15 @@ class ITEMS
     public $quantityInStock;
     public $description;
     public $imageUrl;
+
+    // construct 
+    public function __construct($name, $price, $category, $type, $size, $brand)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->category = $category;
+        $this->type = $type;
+        $this->size = $size;
+        $this->brand = $brand;
+    }
 }
